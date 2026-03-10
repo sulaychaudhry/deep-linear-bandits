e80fff7970d803e40ee97ac9409e48a82520c9f9
Sulay Chaudhry <54546648+sulaychaudhry@users.noreply.github.com>
2026-03-10 19:57:58 +0000

    Simulator will be rewritten, currently messy

--- src/deep_linear_bandits/simulator.py
+++ src/deep_linear_bandits/simulator.py
@@ -3,6 +3,9 @@ import torch
 from deep_linear_bandits.data import KRSmall
 from typing import Protocol
 
+SMALL_USERS = 1411
+SMALL_ITEMS = 3327
+
 # user & item embeddings are for the contextual bandits & also for the greedy policy
 # policies are just classes that the simulator uses to pick what to do with a user that it simulates entering the environment
 # the states are tracked by the policies; it's up to the simulator to just set up those policies correctly here
@@ -18,9 +21,7 @@ class GreedyPolicy(Protocol):
     @torch.no_grad
     def __init__(
             self,
-            small_matrix: KRSmall,
-            user_embeddings: torch.Tensor, # (1411, 64)
-            item_embeddings: torch.Tensor  # (3327, 64)
+            user_item_similarities: torch.Tensor
     ):
         # Compute similarities between users and items
         similarities = user_embeddings @ item_embeddings.T
@@ -29,19 +30,26 @@ class GreedyPolicy(Protocol):
         similarities = torch.sort(similarities, dim=1).indices
 
 class Simulator:
+    @torch.no_grad
     def __init__(
             self,
             small_matrix: KRSmall,
             user_embeddings: torch.Tensor,
             item_embeddings: torch.Tensor
     ):
-        # The linear contextual bandits need a specific feature map of the user & item embeddings
-        # These need precomputing for each user-item pair; the feature map is specifically:
-        #       concat( user embedding, item embedding, element-wise user-item embedding product )
-        u = user_embeddings.unsqueeze(1) # (1411, 1, 64)
-        i = item_embeddings.unsqueeze(0) # (1, 3327, 64)
-        bandit_contexts = torch.cat(
-            u.expand(-1, i.shape[1], -1), # -> (1411, 3327, 64) i.e. broadcast over all items
-            i.expand(u.shape[0], -1, -1), # -> (1411, 3327, 64) i.e. broadcast over all users
-            u * i                         # Automatically broadcasted due to matching 64
-        )
+        # Compute matrix for masking out items that a user doesn't interact with
+        # The small matrix is highly dense, but some interactions are missing
+        available_items = np.zeros((SMALL_USERS, SMALL_ITEMS), dtype=np.bool)
+        available_items[small_matrix.intr_user_ids, small_matrix.intr_item_ids] = True
+
+        # Compute user-item similarity scores for the greedy policy
+        similarities = user_embeddings @ item_embeddings.T
+
+        # Compute matrix necessary for observing rewards;
+
+        # the simulator essentially needs to:
+        # 1. pick a random user (assuming that the user hasn't seen everything)
+        # 2. pass the user to all of the policies
+        # 3. all of the policies need to return their predicted item
+        # 4. the simulator needs to observe the reward for that item
+        # 5. it then loops
